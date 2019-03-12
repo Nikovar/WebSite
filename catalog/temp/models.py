7c4dc8eb96c44e1614f972e13e7c795bf25585ab
Alex P <alex.paru95@gmail.com>
2019-03-12 20:07:51 +0300

    Переезд на новую структуру моделей #2 (#5)
    * Переход на новую структуру моделей.
    
    * Удалил лишние файлы в корне

---
+++ catalog/temp/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from django.core.exceptions import ValidationError
+from django.core.files.storage import FileSystemStorage
+from django.core.validators import FileExtensionValidator
+
+from ..settings import *  # import all db field limits
+from ..models import current_time
+# this will override 'APP_PATH' constant from catalog.settings,
+# so behaviour is proper - will loaded APP_PATH for 'temp' app:
+from .settings import *
+
+book_fs = FileSystemStorage(location=APP_PATH + '/books/')
+
+
+class Pack(models.Model):
+    # explaining of choice for 'max_length' for author:
+    #   all author info in temporary pack is stored into single string in format:
+    #       "first_name<DELIM>last_name<DELIM>xx.xx.xxx<DELIM>yy.yy.yyyy",
+    #   where xx.. = birth date, and yy.. = end date (i.e. date of death, if exist)
+    author_info = models.CharField(max_length=AUTHOR_FST_NAME_LEN + AUTHOR_LST_NAME_LEN + DATE_STRING_REPR_LEN * 2 + DELIM_LEN * 3,
+                                   help_text='Type by format: <first_name>{0}<last_name>{0}<bdate>{0}<edate>'.format(DELIM))
+    book_title = models.CharField(max_length=BOOK_TITLE_LEN)
+    book_genre = models.CharField(max_length=GENRE_NAME_LEN)
+    book_file = models.FileField(storage=book_fs, validators=[FileExtensionValidator(BOOK_EXTENSIONS)])
+    # in respective with following, 'book_extras' will contain book information in format:
+    #       "language<DELIM>summary<DELIM>isbn"
+    book_extras = models.TextField(max_length=LANG_NAME_LEN + SUMMARY_LEN + ISBN_LEN + DELIM_LEN * 2,
+                                   help_text='Type by format: <language>{0}<summary>{0}<isbn>'.format(DELIM))
+
+    inserter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='inserted_temp', related_query_name='ins_tmp')
+    date_joined = models.DateTimeField(default=current_time)
+    checked = models.NullBooleanField(default=None)
+
+    def __str__(self):
+        return self.book_title
+
+    class Meta:
+        ordering = ['book_title']
+
+
+class TempSchema(models.Model):
+    approved = models.NullBooleanField(default=None)
+
+    class Meta:
+        abstract = True
+
+
+class Symbol(TempSchema):
+    occurs_in = models.ForeignKey(Pack, on_delete=models.CASCADE, related_name='symbols', related_query_name='symb')
+    name = models.CharField(max_length=SYMBOL_NAME_LEN)
+
+    def __str__(self):
+        return self.name
+
+
+class Addresses(TempSchema):
+    owner = models.ForeignKey(Symbol, on_delete=models.CASCADE, related_name='addresses', related_query_name='adr')
+
+    start = models.PositiveIntegerField()
+    word_shift = models.PositiveSmallIntegerField()
+    word_len = models.PositiveSmallIntegerField()
+    end_shift = models.PositiveSmallIntegerField()
+
+    def save(self, *args, **kwargs):
+        try:
+            if self.word_shift + self.word_len > self.end_shift:
+                raise ValidationError("Symbol goes outside the existence boards!")
+        except ValidationError as e:
+            print("ERROR! While saving was detected following:\n{}\nSaving aborted.\n".format(e.message))
+        else:
+            super().save(*args, **kwargs)
+
+    def __str__(self):
+        sword_pos = self.start + self.word_shift
+        eword_pos = sword_pos + self.word_len
+        end = self.start + self.end_shift
+        return '({} - [{}..{}] - {})'.format(self.start, sword_pos, eword_pos, end)
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -14,22 +14,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = True
 
-# Set hosts to allow any app on Heroku and the local testing URL
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -40,8 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Add our new application 
-    'catalog.apps.CatalogConfig', #This object was created for us in /catalog/apps.py
+    'catalog.apps.CatalogConfig',
+    'catalog.temp.apps.TempConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +68,6 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -88,8 +77,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -110,15 +97,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
@@ -127,23 +109,7 @@ LOGIN_REDIRECT_URL = '/'
 # Add to test email:
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-# The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
-
-# Static file serving.
-# http://whitenoise.evans.io/en/stable/django.html#django-middleware
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
